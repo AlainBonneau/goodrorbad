7bb636e0681a25d95fbcb8baeb10404f2b5e0e2e
Alain Bonneau <alaindev97@gmail.com>
2025-07-27 10:17:45 +0200

    add new bad messages

--- src/components/GoodOrBad.vue
+++ src/components/GoodOrBad.vue
@@ -116,7 +116,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import "./GoodOrBad.css";
 import messages from "../assets/messages.json";
 
